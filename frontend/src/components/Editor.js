8d3a14bee1dc787f71855923dc812859022bee7f
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-20 14:31:55 +0530

    ediroe page designed

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/matchtags';
 
 const Editor = () => {
-  return (
-    <div>Editor</div>
-  )
-}
+  useEffect(() => {
+    async function init() {
+      CodeMirror.fromTextArea(document.getElementById('realtimeEditor'), {
+        lineNumbers: true,
+        mode: {
+          name: 'javascript',
+          json: true,
+        },
 
-export default Editor
+        theme: 'dracula',
+
+        extraKeys: {
+          'Ctrl-Space': 'autocomplete',
+          'Ctrl-Q': function (cm) {
+            cm.foldCode(cm.getCursor());
+          },
+        },
+        foldGutter: true,
+        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        matchTags: { bothTags: true },
+        lineSeparator: '\n',
+        tabSize: 2,
+        indentUnit: 2,
+        indentWithTabs: true,
+        smartIndent: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea name="" id="realtimeEditor"></textarea>;
+};
+
+export default Editor;
