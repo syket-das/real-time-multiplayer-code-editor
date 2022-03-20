0f01ceeee6e07edfa07c0e7606e06bb6a43c44bf
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-20 13:52:53 +0530

    editor page sidebar --done

--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Client from '../components/Client';
+import Editor from '../components/Editor';
 
 const EditorPage = () => {
-  return (
-    <>Editor Page</>
-  )
-}
+  const [clients, setClients] = useState([
+    {
+      socketId: 1,
+      username: 'Syket Das',
+    },
+    {
+      socketId: 2,
+      username: 'Saurabh',
+    },
+    {
+      socketId: 3,
+      username: 'Syket Das',
+    },
+    {
+      socketId: 4,
+      username: 'Saurabh',
+    },
+  ]);
 
-export default EditorPage
+  return (
+    <>
+      <div className="mainWrap">
+        <div className="aside">
+          <div className="asideInner">
+            <div className="logo">
+              <img className="logoImage" src="/logo192.png" alt="" />
+            </div>
+            <h3>Connected</h3>
+            <div className="clientsList">
+              {clients.map((client) => (
+                <Client key={client.socketId} username={client.username} />
+              ))}
+            </div>
+          </div>
+
+          <button className="btn copyBtn">Copy Room Id</button>
+          <button className="btn leaveBtn">Leave</button>
+        </div>
+        <div className="editorWrap">
+          <Editor />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditorPage;
