4033f657fafd90ebfd6dac9b28ac2b4b53faab31
Syket Das <80612737+syket-das@users.noreply.github.com>
2022-03-20 12:11:01 +0530

    home page html --done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img src="logo192.png" alt="LOGO" />
+        <h4 className="mainLabel">Paste Invitation Room Id</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="Room Id" />
+          <input type="text" className="inputBox" placeholder="User Name" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have a room id, &nbsp;{' '}
+            <a className="createNewBtn" href="#/">
+              create one here
+            </a>
+          </span>
+        </div>
+      </div>
 
-export default Home
+      <footer>
+        <h4>
+          All rights reserved. &copy; 2022 | Developed By{' '}
+          <a target="_blank" rel="noreferrer" href="https://syket-das.me">
+            Syket Das
+          </a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
